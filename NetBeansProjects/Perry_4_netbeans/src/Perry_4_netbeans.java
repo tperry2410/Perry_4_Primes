ca1474f268f77edc7279d0ff1f2e7d8319dffa13
Timothy Perry <tlperry98@gmail.com>
2015-09-15 12:52:16 -0500

    This is my first commit message!

---
+++ NetBeansProjects/Perry_4_netbeans/src/Perry_4_netbeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TimPerry
+ */
+public class Perry_4_netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i = 20; i > 0; i--) {
+        System.out.println("Hello, period " + i + "!");
+        }
+    }
+    
+}
